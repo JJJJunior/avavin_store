965cb0734bc7500d1b9132fb94b0f432c765440e
Maxwell <xj.master@gmail.com>
2024-07-18 16:51:21 +0800

    修改样式

--- app/(root)/search/[query]/page.tsx
+++ app/(root)/search/[query]/page.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import React from "react";
 import axios from "axios";
-import ProductCard from '@/components/ProductCard'
+import ProductCard from "@/components/ProductCard";
 
-const SearchDetailPage = async ({params}: { params: { query: string } }) => {
-
+const SearchDetailPage = async ({ params }: { params: { query: string } }) => {
   const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/search/${params.query}`);
 
   const searchedProducts = res.data;
@@ -11,12 +10,10 @@ const SearchDetailPage = async ({params}: { params: { query: string } }) => {
   return (
     <div className="px-10 py-5">
       <p className="text-heading3-bold my-10">Search results for {params.query}</p>
-      <div className="flex flex-wrap justify-between gap-16">
-        {!searchedProducts || searchedProducts.length === 0 && (
-          <p className="text-body-bold my-5">No result found</p>
-        )}
+      <div className="flex flex-wrap justify-start gap-16">
+        {!searchedProducts || (searchedProducts.length === 0 && <p className="text-body-bold my-5">No result found</p>)}
         {searchedProducts?.map((product: any) => (
-          <ProductCard key={product.id} product={product}/>
+          <ProductCard key={product.id} product={product} />
         ))}
       </div>
     </div>
